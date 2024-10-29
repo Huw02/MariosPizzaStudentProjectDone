686b41e95a5e9022ac4fe1eb0d0e1f9b77e25ee0
huw02 <hannibal@ussing.com>
2024-10-29 08:49:53 +0100

    29-10-2024

--- src/PizzaProgram/Pizza.java
+++ src/PizzaProgram/Pizza.java
@@ -10,15 +10,21 @@ public class Pizza {
 
     public Pizza (){
 
-
+        ValuesOfPizza x = new ValuesOfPizza("", " ", 0);
     }
         ValuesOfPizza margaritha = new ValuesOfPizza("Margaritha", "Tomatsovs og ost", 65);
-        ValuesOfPizza hawaii = new ValuesOfPizza("Hawaii", "Tomatsovs, ost, skinke og ananas", 70);
         ValuesOfPizza pepperoni = new ValuesOfPizza("Pepperoni", "Tomatsovs, ost og pepperoni", 75);
-        ValuesOfPizza meatlover = new ValuesOfPizza("Meatlover", "Tomatsovs, ost, oksekød og pepperoni", 80);
-        ValuesOfPizza bello = new ValuesOfPizza("Bello", "Tomatsovs, ost, svampe og champingoner", 70);
-        ValuesOfPizza mamma = new ValuesOfPizza("Mamma", "Tomatsovs, ost og friske tomater", 90);
-        ValuesOfPizza smello = new ValuesOfPizza("Smello", "Tomatsovs, ost, feta og gorgonzolla", 30);
+        ValuesOfPizza hawaii = new ValuesOfPizza("Hawaii", "Tomatsovs, ost, skinke og ananas", 70);
+        ValuesOfPizza capriccioa = new ValuesOfPizza("Capriccioa", "Tomatsauce, mozzarella, skinke, champignon", 85);
+        ValuesOfPizza quattroStagioni= new ValuesOfPizza("Quattro Stagioni", "Tomatsauce, mozzarella, skinke, champignon, artiskok, oliven ", 90 );
+     ValuesOfPizza vesuvio = new ValuesOfPizza("Vesuvio", "Tomatsauce, mozzarella, skinke ", 80 );
+    ValuesOfPizza diavola = new ValuesOfPizza("Diavola", "Tomatsauce, mozzarella, spicy salami, chili ", 90 );
+    ValuesOfPizza prosciuttoEFunghi = new ValuesOfPizza("Prosciutto e Funghi", " Tomatsauce, mozzarella, skinke, svampe", 85 );
+
+
+
+
+
 
 
 
@@ -27,7 +33,7 @@ public class Pizza {
         boolean kmOn = true;
         while(kmOn) {
             menyMuligheder();
-            System.out.println("Skriv et tal fra 1-8 ud fra den kommando du vil udføre.");
+            System.out.println("\nSkriv et tal fra 1-8 ud fra den kommando du vil udføre.");
             Scanner sc = new Scanner(System.in);
             int scInput = sc.nextInt();
             if (scInput == 1) {
@@ -46,6 +52,7 @@ public class Pizza {
             } else if (scInput == 7) {
 
             } else if (scInput == 8){
+                System.out.println("Du har valgt at slukke for programmet");
                 kmOn = false;
             }else {
                 System.out.println("Jeg forstod ikke hvad du ville, prøve at indtaste et nyt nummer mellem 1-8.");
